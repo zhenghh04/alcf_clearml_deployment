ea701b8381d24fe5cb05ba9c9f5b7abbf080bef4
Huihuo Zheng <zhenghh04@gmail.com>
2026-01-24 18:38:45 +0000

    added different system setup

--- client/launch_clearml_agent.sh
+++ client/launch_clearml_agent.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-export CLEARML_AGENT_SKIP_PIP_VENV_INSTALL="/home/hzheng/clearml/miniconda3/bin/python"
+export CLIENT=${CLIENT:-"sirius"}
+echo "Starting ClearML agent on ALCF with client=${CLIENT}"
+source ${CLIENT}/conda.sh
+export CLEARML_AGENT_SKIP_PIP_VENV_INSTALL="$(which python3)"
 export CLEARML_AGENT_SKIP_PYTHON_ENV_INSTALL=1
 export K8S_GLUE_POD_AGENT_INSTALL_ARGS="==2.0.7rc5"
-export https_proxy=http://proxy.alcf.anl.gov:3128
-export http_proxy=http://proxy.alcf.anl.gov:3128
+
 export PATH=/home/hzheng/clearml/miniconda3/bin/:$PATH
-clearml-agent-slurm --template-files ./pbs.template --queue default --use-pbs
+
+clearml-agent-slurm --template-files ${CLIENT}/pbs.template --queue default --use-pbs
---
+++ client/sirius/conda.sh
@@ -0,0 +1,4 @@
+export https_proxy=http://proxy.alcf.anl.gov:3128
+export http_proxy=http://proxy.alcf.anl.gov:3128
+module load conda
+conda activate $HOME/argonne_tpc/clearml/venvs/clearml-py10
