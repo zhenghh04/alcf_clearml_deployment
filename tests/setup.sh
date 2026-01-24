8e5f92a0cd75f8dc9915bcddfd3f0acebf1e260f
Huihuo Zheng <huihuo.zheng@anl.gov>
2026-01-24 12:31:57 -0600

    added examples

---
+++ tests/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export OPENBLAS_NUM_THREADS=1
+export CLEARML_AGENT_SKIP_PIP_VENV_INSTALL="/home/hzheng/clearml/miniconda3/bin/python"
+export CLEARML_AGENT_SKIP_PYTHON_ENV_INSTALL=1
+export K8S_GLUE_POD_AGENT_INSTALL_ARGS="==2.0.7rc5"
+export https_proxy=http://proxy.alcf.anl.gov:3128
+export http_proxy=http://proxy.alcf.anl.gov:3128
+export PATH=/home/hzheng/clearml/miniconda3/bin/:$PATH
+#clearml-agent-slurm --template-files ./pbs.template --queue default --use-pbs
