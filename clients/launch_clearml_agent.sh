77db229b616651f99519dd2291d7852f98f3a999
Huihuo Zheng <huihuo.zheng@anl.gov>
2026-01-25 00:57:03 -0600

    fix pbs template

--- clients/launch_clearml_agent.sh
+++ clients/launch_clearml_agent.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 export CLIENT=${CLIENT:-"sirius"}
-echo "Starting ClearML agent on ALCF with client=${CLIENT}"
+echo "Starting ClearML agent on ALCF with client: ${CLIENT}"
 source ${CLIENT}/conda.sh
 export CLEARML_AGENT_SKIP_PIP_VENV_INSTALL="$(which python3)"
 export CLEARML_AGENT_SKIP_PYTHON_ENV_INSTALL=1
 export K8S_GLUE_POD_AGENT_INSTALL_ARGS="==2.0.7rc5"
-
-export PATH=/home/hzheng/clearml/miniconda3/bin/:$PATH
 
 clearml-agent-slurm --template-files ${CLIENT}/pbs.template --queue ${QUEUE:-$CLIENT} --use-pbs
--- tests/job_launching/test.py
+++ tests/job_launching/test.py
@@ -2,5 +2,3 @@
 import time
 import os
 print("start running")
-nodefile = os.environ['PBS_NODEFILE']
-os.system(f"mpiexec -np 128 --hostfile={nodefile} /home/hzheng/clearml/pi_mpi4py.py --samples 100000000")
