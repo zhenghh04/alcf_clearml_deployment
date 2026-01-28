739d1c73a31a8601c71d34bed19f60d475218692
Huihuo Zheng <zhenghh04@gmail.com>
2026-01-28 10:38:42 -0600

    add more log

--- tests/pipeline/multi_facilities/tasks/evaluate.sh
+++ tests/pipeline/multi_facilities/tasks/evaluate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "  Evaluating model..."
+echo "`date`  Evaluating model..."
--- tests/pipeline/multi_facilities/tasks/prepare_data.sh
+++ tests/pipeline/multi_facilities/tasks/prepare_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "`date`  Preparing data..."
+echo "Including downloading dataset; preprocessing ..."
 export DST=/eagle/datascience/hzheng/clearml/data/cifar10/
 mkdir -p $DST
 wget -c -P $DST https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
