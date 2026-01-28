a407c127071afb438e52c868d763e714c7974ef1
Huihuo Zheng <zhenghh04@gmail.com>
2026-01-27 23:14:43 -0600

    login

--- tests/job_launching/bash/test_login.py
+++ tests/job_launching/bash/test_login.py
@@ -3,9 +3,11 @@ from clearml import Task
 created_task = Task.create(
     project_name = "AmSC",
     task_name = "Workflow login",
-    script = "./run_login.sh",
+    repo="https://github.com/zhenghh04/alcf_clearml_evaluation.git",
+    branch="main",
+    script = "tests/job_launching/bash/run_login.sh",
+    working_directory = "./",
     binary = "/bin/bash",
-    force_single_script_file = True
 )
 
 Task.enqueue(created_task, queue_name="sirius-login")
