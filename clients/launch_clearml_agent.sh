0f134e54c1fcabee4dd6a082c44f7b118ba7dd4d
Huihuo Zheng <zhenghh04@gmail.com>
2026-01-29 00:20:48 -0600

    added git user

--- clients/launch_clearml_agent.sh
+++ clients/launch_clearml_agent.sh
@@ -7,6 +7,8 @@ export CLEARML_AGENT_SKIP_PIP_VENV_INSTALL="$(which python3)"
 export CLEARML_AGENT_SKIP_PYTHON_ENV_INSTALL=1
 export K8S_GLUE_POD_AGENT_INSTALL_ARGS="==2.0.7rc5"
 
+[[ -e ../.env ]] && echo "Setting environment variables in ../.env"
+
 # Kill existing agent
 pkill clearml-agent-slurm
 pkill clearml-agent
