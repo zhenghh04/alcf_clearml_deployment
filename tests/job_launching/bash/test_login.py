9e596c2434f852ac0f9a4eb1116ef874b446a72a
Huihuo Zheng <zhenghh04@gmail.com>
2026-01-29 00:49:51 -0600

    test pbs

--- tests/job_launching/bash/test_login.py
+++ tests/job_launching/bash/test_login.py
@@ -2,13 +2,13 @@ from clearml import Task
 import argparse
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--queue", default="crux")
+parser.add_argument("--queue", default="crux-login")
 args = parser.parse_args()
 
 created_task = Task.create(
     project_name = "AmSC",
     task_name = "Workflow login",
-    repo="https://github.com/zhenghh04/alcf_clearml_evaluation.git",
+    repo="git@github.com:zhenghh04/alcf_clearml_evaluation.git",
     branch="main",
     script = "tests/job_launching/bash/run_login.sh",
     working_directory = "./",
