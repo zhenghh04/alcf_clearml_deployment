0a1cc69b9d8999cebb2d66ca56452c8c48a2b438
Huihuo Zheng <zhenghh04@gmail.com>
2026-01-29 00:18:58 -0600

    added new env setup

--- clients/install_clearml.sh
+++ clients/install_clearml.sh
@@ -1,5 +1,7 @@
-#!/bin/bash +x
-pip install -U --extra-index-url https://USERNAME:PASSWORD@packages.allegro.ai/repository/clearml_agent_slurm/simple \
+#!/bin/bash -x
+
+[[ -e ../.env ]] && source ../.env || echo "please define CLEARML_PIP_PACKAGE_USER and CLEARML_PIP_PACKAGE_PASS in ../.env\n Your credentials for --extra-index-url are available in the WebApp under the Help menu Help menu > ClearML Python Package setup > Install step."
+pip install -U --extra-index-url https://${CLEARML_PIP_PACKAGE_USER}:${CLEARML_PIP_PACKAGE_PASS}@packages.allegro.ai/repository/clearml_agent_slurm/simple \
     clearml-agent-slurm==0.10.1 \
     clearml-agent==2.0.7rc5
 
