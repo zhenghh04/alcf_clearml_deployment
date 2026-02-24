66463ac41fe060e2e57e99684a640cf5e89a71de
Huihuo Zheng <zhenghh04@gmail.com>
2026-02-24 12:43:01 -0600

    update task

--- clients/aurora/conda.sh
+++ clients/aurora/conda.sh
@@ -1,4 +1,5 @@
 export https_proxy=http://proxy.alcf.anl.gov:3128
 export http_proxy=http://proxy.alcf.anl.gov:3128
 module load frameworks
-source $HOME//clearml/alcf_clearml_evaluation/clients/aurora/venvs/clearml/bin/activate
+source $HOME//clearml/alcf_clearml_evaluation/clients/aurora/envs/clearml/bin/activate
+export PATH=/home/hzheng/.local/aurora/frameworks/2025.2.0/bin:$PATH
