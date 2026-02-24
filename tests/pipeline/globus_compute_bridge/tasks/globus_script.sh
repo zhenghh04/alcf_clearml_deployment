8766e5ed35a610e6385a5592f5e70f860223f68c
Huihuo Zheng <zhenghh04@gmail.com>
2026-02-24 12:23:12 -0600

    added jobs

--- tests/pipeline/globus_compute_bridge/tasks/globus_script.sh
+++ tests/pipeline/globus_compute_bridge/tasks/globus_script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-nvidia-smi
+echo "Started job on $(hostname) at $(date)"
+echo "Running a dummy script on the Globus Compute endpoint..."
 echo "Sleeping for 10 seconds to simulate a long-running task..."
-python -c "import time; time.sleep(10)"
+echo "Running on nodes: $PBS_NODEFILE"
+python -c "import time; time.sleep(10); import socket; print('Hostname:', socket.gethostname())"
+export PBS_JOBSIZE=$(cat $PBS_NODEFILE | uniq | wc -l)
+export PPN=4
+mpiexec -n $((PBS_JOBSIZE*PPN)) --ppn $PPN python -c "from mpi4py import MPI; import socket; comm = MPI.COMM_WORLD; rank = comm.Get_rank(); print(f'Rank {rank} on {socket.gethostname()}')"
 echo "Done sleeping, exiting now."
