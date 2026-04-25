25b4af8886d7b675a297f16e0b8ef2dffbb4dd7e
Huihuo Zheng <huihuo.zheng@anl.gov>
2026-04-25 01:15:13 -0400

    remove clearm.conf overwrite

--- clients/clearml_agent_setup/perlmutter/conda.sh
+++ clients/clearml_agent_setup/perlmutter/conda.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
 module load python/3.13
 VENV_DIR=$HOME/clearml/alcf_clearml_deployment/clients/clearml_agent_setup/perlmutter/envs/clearml/
 source "${VENV_DIR}/bin/activate"
